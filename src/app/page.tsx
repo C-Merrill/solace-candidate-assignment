65be8d80e951b7a4ee26549fc699e2a550e781cd
Chris <cmerrill99@gmail.com>
2025-03-19 14:43:38 -0500

    Remove search/reset buttons Make search case insensitive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,18 +18,16 @@ export default function Home() {
   }, []);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term")!.innerHTML = searchTerm;
+    const searchTerm = e.target.value.toLowerCase();
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
-      const specialtiesSearchString = advocate.specialties.join(';');
+      const specialtiesSearchString = advocate.specialties.join(';').toLowerCase();
       return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
+        advocate.firstName.toLowerCase().includes(searchTerm) ||
+        advocate.lastName.toLowerCase().includes(searchTerm) ||
+        advocate.city.toLowerCase().includes(searchTerm) ||
+        advocate.degree.toLowerCase().includes(searchTerm) ||
         specialtiesSearchString.includes(searchTerm) ||
         advocate.yearsOfExperience.toString().includes(searchTerm)
       );
@@ -38,23 +36,13 @@ export default function Home() {
     setFilteredAdvocates(filteredAdvocates);
   };
 
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
-  };
-
   return (
     <main style={{ margin: "24px" }}>
       <h1>Solace Advocates</h1>
       <br />
       <br />
       <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+        <input style={{ border: "1px solid black" }} placeholder="Search" onChange={onChange} />
       </div>
       <br />
       <br />
@@ -73,14 +61,14 @@ export default function Home() {
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr key={advocate.id}>
+              <tr key={advocate.id.toString()}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
                   {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                    <div key={`${advocate.id}_${s}`}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
